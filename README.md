3cc6fc2f0251775e87e3dc49bc22363b8bcca917
Kyle King <kmking72@gmail.com>
2014-09-05 06:38:29 -0400

    Basic map location of user
    and added sources for help in readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ testProject
 
 > Nothing to see here
 
-1. Install Meteor ([Guide](http://docs.meteor.com))
+1. Install Meteor ([Guide ](http://docs.meteor.com))
 
   ```
   curl https://install.meteor.com | /bin/sh
@@ -21,5 +21,10 @@ testProject
   meteor
   ```
 
-4. Point your browser to [Localhost:3000](http://localhost:3000)
+4. Point your browser to [Localhost:3000 ](http://localhost:3000)
 5. If it doesn't work, let me know!
+
+### Tutorials, StackOverflow - basically everything that has kept this project alive:
+1. [Great explanation of loading behavior of Meteor ](http://www.kingkryn.com/?p=1697#comment-5018)
+2. [Got me started with Meteor/Leaflet and added find my location ](http://www.kingkryn.com/?p=1697#comment-5018)
+3. [Adding markers - useful for incorporating marker layers later on]()
--- client/main.js
+++ client/main.js
@@ -1,26 +1,25 @@
 // Upon loading the map template, runs this code:
 Template.map.rendered = function() {
-
 };
 
 // Wait for subscription to plot bikes data
 Meteor.subscribe("bikesData", function() { // Callback fired when data received
-  if (Meteor.isClient) { // double check to make sure Meteor is sending data
+  if (Meteor.isClient) { // double check to make sure Meteor is client
+    // Start Minimum working code:
+    var map = L.map('map').setView([38.987701, -76.940989], 14); // Center on Mckeldin
+    L.tileLayer('http://{s}.tiles.mapbox.com/v3/kyleking.icaokpd6/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 16,
+        minZoom: 12
+    }).addTo(map);
+    // End Minimum working code:
+
       var bikesData = Bikes.find().fetch(); // Fetch collection data
 
     // Use Jquery to proper;y size the map for any browser
     // var w = window.innerWidth;
     // var h = window.innerHeight;
     // $('#map_container').css({width: w+'px', height: h+'px'});
-
-    // Start Minimum working code:
-      var map = L.map('map').setView([bikesData[0].latitude, bikesData[0].longitude], 14); // Center on Bike #1
-      L.tileLayer('http://{s}.tiles.mapbox.com/v3/kyleking.icaokpd6/{z}/{x}/{y}.png', {
-          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-          maxZoom: 16,
-          minZoom: 12
-      }).addTo(map);
-    // End Minimum working code:
 
     // Plot bikes as circles
     for (var i = (bikesData.length-1); i >= 0; i--) {
@@ -31,6 +30,7 @@ Meteor.subscribe("bikesData", function() { // Callback fired when data received
             fillColor: '#43ff6c',
             fillOpacity: 0.9
           }).addTo(map);
+        // console.log(bikesData[i].status); Should print out 11 "good's"
       } else { // If bad status, make red circle
         var circle = L.circle([bikesData[i].latitude, bikesData[i].longitude], 25, {
             color: 'red',
@@ -42,4 +42,18 @@ Meteor.subscribe("bikesData", function() { // Callback fired when data received
     } else {
     console.log('loading');
   };
+    map.locate({setView: true})
+     .on('locationfound', function(e) {
+      var marker = L.marker([e.latitude, e.longitude]).addTo(map);
+    });
+     // var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+     // color: 'green',
+     // fillColor: '#f03',
+     // fillOpacity: 0.5
+     // });
+     // map.addLayer(circle);
+     // })
+     // .on('locationerror', function(e) {
+     // map = L.map('map').setView([30.28, -97.73], 13);
+     // });
 });
