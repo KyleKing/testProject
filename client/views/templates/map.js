ed8605592ff12a1a29be87b0bbcf27f1a4f798a0
Kyle King <kmking72@gmail.com>
2014-11-02 13:50:13 -0500

    scorched earth update to D3 Demo files

--- client/views/templates/map.js
+++ client/views/templates/map.js
@@ -9,9 +9,30 @@ Template.map.rendered = function() {
 
       map = new L.Map('map', {
         center: new L.LatLng(38.987701, -76.940989),
-        zoom: 17,
+        maxZoom: 19,
+        zoom: 16,
         zoomControl: false
       });
+
+
+      // var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+      //   denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
+      //   aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
+      //   golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+      // var cities = L.layerGroup([littleton, denver, aurora, golden]);
+
+      // var markers = new L.MarkerClusterGroup();
+      // markers.addLayer( new L.Marker(new L.LatLng(38.987701, -76.940969) ) );
+      // map.addLayer(markers);
+
+      // var markers = L.markerClusterGroup();
+      // var points_rand = L.geoJson(points, {
+      //     onEachFeature: function (feature, layer) //functionality on click on feature
+      //         {
+      //         layer.bindPopup("hi! I am one of thousands"); //just to show something in the popup. could be part of the geojson as well!
+      //         }
+      // });
+
 
       OpenCycleMap = L.tileLayer("http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png", {
         attribution: "&copy; <a href=\"http://www.opencyclemap.org\">OpenCycleMap</a>, &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>"
@@ -23,28 +44,42 @@ Template.map.rendered = function() {
 
       map.addControl(zoomControl);
 
+      // Use Leaflet cluster group plugin
+      var markers = new L.MarkerClusterGroup();
       bikesData = Bikes.find().fetch();
 
       i = bikesData.length - 1;
       while (i >= 0) {
         if (bikesData[i].status === "Good") {
-          circle = L.circle([bikesData[i].latitude, bikesData[i].longitude], 10, {
-            color: "green",
-            fillColor: "#43ff6c",
-            fillOpacity: 0.9
-          }).addTo(map);
+          markers.addLayer( new L.Marker(new L.LatLng(bikesData[i].latitude, bikesData[i].longitude) ) );
         } else {
-          circle = L.circle([bikesData[i].latitude, bikesData[i].longitude], 30, {
-            color: "red",
-            fillColor: "#f03",
-            fillOpacity: 0.9
-          }).addTo(map);
+          console.log("Bad Bike");
         }
         i--;
       }
-    } else {
-      console.log("loading");
-    }
+
+      map.addLayer(markers);
+
+    //   i = bikesData.length - 1;
+    //   while (i >= 0) {
+    //     if (bikesData[i].status === "Good") {
+    //       circle = L.circle([bikesData[i].latitude, bikesData[i].longitude], 10, {
+    //         color: "green",
+    //         fillColor: "#43ff6c",
+    //         fillOpacity: 0.9
+    //       }).addTo(map);
+    //     } else {
+    //       circle = L.circle([bikesData[i].latitude, bikesData[i].longitude], 30, {
+    //         color: "red",
+    //         fillColor: "#f03",
+    //         fillOpacity: 0.9
+    //       }).addTo(map);
+    //     }
+    //     i--;
+    //   }
+    // } else {
+    //   console.log("loading");
+    // }
 
     map.locate({
       setView: true
@@ -53,6 +88,7 @@ Template.map.rendered = function() {
     var marker;
       marker = L.marker([e.latitude, e.longitude]).addTo(map);
     });
-  });
+  };
+});
 };
 
