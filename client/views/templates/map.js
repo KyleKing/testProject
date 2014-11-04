4cca7e89506b8fe6b3cc90417c10b187b0c6bb60
Kyle King <kmking72@gmail.com>
2014-11-04 16:32:11 -0500

    added plugins, camera and geo and moment.js

--- client/views/templates/map.js
+++ client/views/templates/map.js
@@ -92,3 +92,14 @@ Template.map.rendered = function() {
 });
 };
 
+
+// Not working and not sure why?
+// if (Meteor.isClient) {
+//   Template.body.helpers({
+//     loc: function () {
+//       // return 0, 0 if the location isn't ready
+//       return Geolocation.latLng() || { lat: 0, lng: 0 };
+//     },
+//     error: Geolocation.error
+//   });
+// }
