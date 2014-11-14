bb1bc08ea169d30e7905ae48140ebff36c77c042
Kyle King <kmking72@gmail.com>
2014-11-14 13:07:41 -0500

    Removed heatmap.js

--- client/views/templates/profile/TempMapProfile.js
+++ client/views/templates/profile/TempMapProfile.js
@@ -120,57 +120,60 @@ Template.profile.created = function() {
 
 
 
-        // standard gmaps initialization
-    var myLatlng = new google.maps.LatLng(48.3333, 16.35);
+    //     // standard gmaps initialization
+    // var myLatlng = new google.maps.LatLng(48.3333, 16.35);
 
-    // define map properties
-    var myOptions = {
-        zoom: 3,
-        center: myLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: false,
-        scrollwheel: true,
-        draggable: true,
-        navigationControl: true,
-        mapTypeControl: false,
-        scaleControl: true,
-        disableDoubleClickZoom: false
-    };
+    // // define map properties
+    // var myOptions = {
+    //     zoom: 3,
+    //     center: myLatlng,
+    //     mapTypeId: google.maps.MapTypeId.ROADMAP,
+    //     disableDefaultUI: false,
+    //     scrollwheel: true,
+    //     draggable: true,
+    //     navigationControl: true,
+    //     mapTypeControl: false,
+    //     scaleControl: true,
+    //     disableDoubleClickZoom: false
+    // };
 
-    // we'll use the heatmapArea 
-    var map = new google.maps.Map($("#heatmapArea")[0], myOptions);
+    // // we'll use the heatmapArea 
+    // var map = new google.maps.Map($("#heatmapArea")[0], myOptions);
 
-    // let's create a heatmap-overlay
-    // with heatmap config properties
-    var heatmap = new HeatmapOverlay(map, {
-        "radius": 20,
-            "visible": true,
-            "opacity": 60
-    });
+    // // let's create a heatmap-overlay
+    // // with heatmap config properties
+    // var heatmap = new HeatmapOverlay(map, {
+    //     "radius": 20,
+    //         "visible": true,
+    //         "opacity": 60
+    // });
 
-    // here is our dataset
-    // important: a datapoint now contains lat, lng and count property!
-    var testData = {
-        max: 3,
-        data: [{
-            lat: 48.3333,
-            lng: 16.35,
-            count: 100
-        }, {
-            lat: 51.465558,
-            lng: 0.010986,
-            count: 100
-        }, {
-            lat: 33.5363,
-            lng: -5.044,
-            count: 100
-        }]
-    };
+    // // here is our dataset
+    // // important: a datapoint now contains lat, lng and count property!
+    // var testData = {
+    //     max: 3,
+    //     data: [{
+    //         lat: 48.3333,
+    //         lng: 16.35,
+    //         count: 100
+    //     }, {
+    //         lat: 51.465558,
+    //         lng: 0.010986,
+    //         count: 100
+    //     }, {
+    //         lat: 33.5363,
+    //         lng: -5.044,
+    //         count: 100
+    //     }]
+    // };
 
-    // now we can set the data
-    google.maps.event.addListenerOnce(map, "idle", function () {
-        // this is important, because if you set the data set too early, the latlng/pixel projection doesn't work
-        //debugger;
-        heatmap.setDataSet(testData);
-    });
+    // // now we can set the data
+    // google.maps.event.addListenerOnce(map, "idle", function () {
+    //     // this is important, because if you set the data set too early, the latlng/pixel projection doesn't work
+    //     //debugger;
+    //     heatmap.setDataSet(testData);
+    // });
+
+  
+
 };
