b107379ee2d830750dfd812f2d1a31485f887238
Kyle King <kmking72@gmail.com>
2014-11-20 16:11:50 -0500

    Fixed mechanic button and added slide content left

--- client/views/layouts/mobileNav.js
+++ client/views/layouts/mobileNav.js
@@ -1,12 +1,14 @@
 Template.menu.rendered = function() {
   $(document).ready(function(){
     $('.js-menu-trigger').on('click touchstart', function(e){
+      $('.content').toggleClass('is-hidden');
       $('.js-menu').toggleClass('is-visible');
       $('.js-menu-screen').toggleClass('is-visible');
       e.preventDefault();
     });
 
     $('.js-menu-screen').on('click touchstart', function(e){
+      $('.content').toggleClass('is-hidden');
       $('.js-menu').toggleClass('is-visible');
       $('.js-menu-screen').toggleClass('is-visible');
       e.preventDefault();
