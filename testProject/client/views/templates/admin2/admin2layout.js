472d9673dcf4aa789430613812138d6bbf5d1317
Kyle King <kmking72@gmail.com>
2015-03-02 10:47:51 -0500

    User list with additional aggregated data

--- testProject/client/views/templates/admin2/admin2layout.js
+++ testProject/client/views/templates/admin2/admin2layout.js
@@ -1,10 +1,43 @@
 Meteor.subscribe("TestUsersData");
 Meteor.call('eachBike');
 
+Template.admin2layout.created = function() {
+  console.log("Created!");
+  Session.set('ViewUsers', 0);
+};
+
 Template.admin2layout.helpers({
   admin2layout: function () {
-    return TestUsers.find().fetch(); // (i.e. findOne)
-    // return TestUsers.findOne();
-    // return TestUsers.find({bike: 8}).fetch()[0]; // (i.e. findOne)
+    // Return all users in system
+    if (Session.get('ViewUsers') === 0) {
+      return TestUsers.find().fetch();
+    } else {
+      // Return only the clicked user:
+      // return TestUsers.find({bike: 7}).fetch()[0]; // (i.e. findOne)
+      return TestUsers.findOne({_id: Session.get('ViewUsers')});
+    }
+  },
+  // Determing which view to return (true = all, false = single user)
+  ViewUsersFunc: function () {
+    if (Session.get('ViewUsers') === 0) {
+      // console.log('True: ' + Session.get('ViewUsers'));
+      return false;
+    } else {
+      // console.log('False: ' + Session.get('ViewUsers'));
+      return true;
+    }
+  }
+});
+
+// open the particular users history
+Template.admin2layout.events({
+  'click .seeMore': function () {
+    Session.set('ViewUsers', this._id);
+  }
+});
+// Return to the main layout view
+Template.admin2layout.events({
+  'click .seeLess': function () {
+    Session.set('ViewUsers', 0);
   }
 });
