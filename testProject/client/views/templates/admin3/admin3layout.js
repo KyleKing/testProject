fe2fb25e450351fe8710c9b4252d910e6002cf12
Kyle King <kmking72@gmail.com>
2015-03-02 11:48:11 -0500

    Per bike use

---
+++ testProject/client/views/templates/admin3/admin3layout.js
@@ -0,0 +1,42 @@
+Meteor.subscribe("TestUsersData");
+Meteor.call('eachBike');
+
+Template.admin3layout.created = function() {
+  // Default to all users view
+  Session.set('ViewUsers', 0);
+};
+
+Template.admin3layout.helpers({
+  admin3layout: function () {
+    // Return all bikes in system
+    if (Session.get('ViewUsers') === 0) {
+      return TimeSeries.find().fetch();
+    } else {
+      // Return only the clicked bike:
+      return TimeSeries.findOne({_id: Session.get('ViewUsers')});
+    }
+  },
+  // Determing which view to return (true = all, false = single user)
+  ViewUsersFunc: function () {
+    if (Session.get('ViewUsers') === 0) {
+      // console.log('True: ' + Session.get('ViewUsers'));
+      return false;
+    } else {
+      // console.log('False: ' + Session.get('ViewUsers'));
+      return true;
+    }
+  }
+});
+
+// open the particular users history
+Template.admin3layout.events({
+  'click .seeMore': function () {
+    Session.set('ViewUsers', this._id);
+  }
+});
+// Return to the main layout view
+Template.admin3layout.events({
+  'click .seeLess': function () {
+    Session.set('ViewUsers', 0);
+  }
+});
