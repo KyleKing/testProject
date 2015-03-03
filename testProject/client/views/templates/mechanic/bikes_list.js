898b543a022bf1c61a3bebcb56f2d88917437618
Kyle King <kmking72@gmail.com>
2015-03-03 07:46:01 -0500

    Form for mechanic bikes list

--- testProject/client/views/templates/mechanic/bikes_list.js
+++ testProject/client/views/templates/mechanic/bikes_list.js
@@ -1,9 +1,22 @@
 Meteor.subscribe("bikesData");
 
+Template.bikesList.created = function() {
+  // Default to all users view
+  Session.set('BikeNumber', 1);
+};
+
+Template.bikesList.events({
+    'submit form': function(event){
+    event.preventDefault();
+    var BikeNumber = parseInt(event.target.BikeNumber.value);
+    Session.set('BikeNumber', BikeNumber);
+    console.log(_.isString(BikeNumber));
+    }
+});
+
 Template.bikesList.helpers({
   bikes: function () {
     if (Bikes.findOne({bike: 1})) {
-      Session.set('BikeNumber', 1);
       // this helper returns a cursor of all of the posts in the collection
       var bikeData = Bikes.findOne({bike: Session.get('BikeNumber')}).updates;
       return _.sortBy(bikeData, "timestamp").reverse();
