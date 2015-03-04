e8355a26233bba38aa16e22f6873ad78a7db6849
Kyle King <kmking72@gmail.com>
2015-03-04 00:06:22 -0500

    Working mechanic sort list

--- testProject/client/views/templates/mechanic/bikes_list.js
+++ testProject/client/views/templates/mechanic/bikes_list.js
@@ -6,7 +6,7 @@ Template.bikesList.created = function() {
   Session.set('BikeNumber', 1);
   Session.set('sortType', 'timestamp');
   Session.set('sortOrder', 'desc');
-  Session.set('mechanic', 'Doretha Bayne');
+  Session.set('mechanic', '');
 };
 
 // Source: http://stackoverflow.com/questions/604167/how-can-we-access-the-value-of-a-radio-button-using-the-dom
@@ -35,42 +35,42 @@ Template.bikesList.helpers({
 
 Template.bikesList.events({
     'submit form': function(event){
-    event.preventDefault();
-    var BikeNumber = parseFloat(event.target.BikeNumber.value);
-    Session.set('BikeNumber', BikeNumber);
-    // console.log(_.isString(BikeNumber));
-    // var sortOrder = event.target.sortOrder;
-    Session.set('sortOrder', getRadioValue('sortOrder'));
-    // console.log(getRadioValue('sortOrder'));
-    Session.set('sortType', event.target.sortType.value);
-    Session.set('mechanic', event.target.mechanicSelected.value);
-    console.log('mechanic = ' + event.target.mechanicSelected.value);
+      event.preventDefault();
+      var BikeNumber = parseFloat(event.target.BikeNumber.value);
+      Session.set('BikeNumber', BikeNumber);
+      // console.log(_.isString(BikeNumber));
+      // var sortOrder = event.target.sortOrder;
+      Session.set('sortOrder', getRadioValue('sortOrder'));
+      // console.log(getRadioValue('sortOrder'));
+      Session.set('sortType', event.target.sortType.value);
+      Session.set('mechanic', event.target.mechanicSelected.value);
+      console.log('mechanic = ' + event.target.mechanicSelected.value);
+      // bikes.call();
     }
 });
 
-Template.bikesList.helpers({
-  bikes: function () {
-    if (Bikes.findOne({bike: Session.get('BikeNumber')})) {
-      // this helper returns a cursor of all of the posts in the collection
-      var bikeData = Bikes.findOne({bike: Session.get('BikeNumber')}).updates;
-      if (Session.get('mechanic')) {
-        console.log('Session');
-        bikeData = _.filter(bikeData, function(bikes){ return bikes.mechanic === Session.get('mechanic'); });
-        // console.log(Session.get('mechanic'));
-        console.log(bikeData);
-      } else {
-        console.log('No Session');
-      }
 
-      if (Session.get('sortOrder') === 'desc') {
-        console.log(bikeData);
-        return _.sortBy(bikeData, Session.get('sortType')).reverse();
-      } else {
-        return _.sortBy(bikeData, Session.get('sortType'));
+// Tracker.autorun(function () {
+  Template.bikesList.helpers({
+    bikes: function () {
+      if (Bikes.findOne({bike: Session.get('BikeNumber')})) {
+        // this helper returns a cursor of all of the posts in the collection
+        var bikeData = Bikes.findOne({bike: Session.get('BikeNumber')}).updates;
+
+        // Return only specific mechanic
+        if (Session.get('mechanic')) {
+          bikeData = _.filter(bikeData, function(bikes){ return bikes.mechanic === Session.get('mechanic'); });
+        } else { console.log('No Session'); }
+
+        // Order results for table (desc vs. asc)
+        if (Session.get('sortOrder') === 'desc') {
+          // console.log(bikeData);
+          return _.sortBy(bikeData, Session.get('sortType')).reverse();
+        } else { return _.sortBy(bikeData, Session.get('sortType')); }
       }
     }
-  }
-});
+  });
+// });
 
 Template.bikesList.helpers({
   mechanicName: function () {
