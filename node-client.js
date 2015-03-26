e34c387f4091147d919f7f4004d09b034f308ee0
Kyle King <kmking72@gmail.com>
2015-03-26 16:16:47 -0400

    Remote DDP Connection! Just need SSL now

--- node-client.js
+++ node-client.js
@@ -9,26 +9,26 @@ var moment = require('moment');
 moment().format();
 
 // Remote connections:
-// var ddpclient = new DDPClient({
-//   host: "gw2event.meteor.com",
-//   port: 80,
-//   auto_reconnect: true,
-//   auto_reconnect_timer: 500
-// });
+var ddpclient = new DDPClient({
+  host: "teambikes.me",
+  port: 80,
+  auto_reconnect: true,
+  auto_reconnect_timer: 500
+});
 // Source: https://github.com/oortcloud/node-ddp-client/issues/21
 
 // Connect to Meteor
-var ddpclient = new DDPClient({
-  host: "localhost",
-  port: 3000,
-  /* optional: */
-  auto_reconnect: true,
-  auto_reconnect_timer: 500,
-  use_ejson: true,  // default is false
-  use_ssl: false, //connect to SSL server,
-  use_ssl_strict: true, //Set to false if you have root ca trouble.
-  maintain_collections: true //Set to false to maintain your own collections.
-});
+// var ddpclient = new DDPClient({
+//   host: "localhost",
+//   port: 3000,
+//   /* optional: */
+//   auto_reconnect: true,
+//   auto_reconnect_timer: 500,
+//   use_ejson: true,  // default is false
+//   use_ssl: false, //connect to SSL server,
+//   use_ssl_strict: true, //Set to false if you have root ca trouble.
+//   maintain_collections: true //Set to false to maintain your own collections.
+// });
 
 ddpclient.connect(function(error) {
   // Error Checking
